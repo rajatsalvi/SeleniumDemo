1cef392e6a3631d69f6562f46cec15a6d2bdaeb2
rajatsalvi <rajatsalvi6@gmail.com>
2019-07-01 19:05:35 +0530

    changes have made

--- src/formfill/Form.java
+++ src/formfill/Form.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,11 +15,11 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Form {
-
+	static WebDriver wd;
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\\\Users\\\\hemant\\\\Downloads\\\\New folder/chromedriver.exe");
-	      WebDriver wd = new ChromeDriver();
+	      wd = new ChromeDriver();
 	      wd.get("http://www.gcrit.com/build3/create_account.php");
 	      Thread.sleep(2000);
 	      wd.findElement(By.xpath("//*[@id=\"bodyContent\"]/form/div/div[2]/table/tbody/tr[1]/td[2]/input[1]")).click();
@@ -27,10 +28,17 @@ public class Form {
 	      wd.findElement(By.id("dob")).sendKeys("11/15/1996",Keys.ENTER);
 	      
 	     
+	      try {
+	    	  if(isAlertPresent()) {
+	    		  System.out.println("Alert was there");
+	    	  }
+	    	  
+	      }catch (NoSuchElementException ex) {
+	    	  System.out.println("Alert was not there");
+	      }
 	      
-	      WebDriverWait wait = new WebDriverWait(wd,10);
-	      wait.until(ExpectedConditions.alertIsPresent());
-	      wd.switchTo().alert().accept();
+	      
+	     
 	      
 	      wd.findElement(By.name("email_address")).sendKeys("rajatsalvi6@gmail.com");
 	      wd.findElement(By.name("company")).sendKeys("Karma");
@@ -54,6 +62,14 @@ public class Form {
 	      wd.navigate().to("https://dell.com");
 	      wd.quit();
 		}
+
+	private static boolean isAlertPresent() {
+		// TODO Auto-generated method stub
+		 WebDriverWait wait = new WebDriverWait(wd,10);
+	      wait.until(ExpectedConditions.alertIsPresent());
+	      wd.switchTo().alert().accept();
+		return true;
+	}
 
 	}
 
