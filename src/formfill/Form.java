a785a3ad2efd9e63caeb70660275ad3608922e4c
rajatsalvi <rajatsalvi6@gmail.com>
2019-07-03 10:57:10 +0530

    changes done

--- src/formfill/Form.java
+++ src/formfill/Form.java
@@ -44,54 +44,7 @@ public class Form {
 	      wd.findElement(By.id("dob")).sendKeys("11/15/1996",Keys.ENTER);
 	      
 	     
-//	      try {
-//	    	  if(isAlertPresent()) {
-//	    		  System.out.println("Alert was there");
-//	    	  }
-//	    	  WebDriverWait wait = new WebDriverWait(wd,10);
-//		      wait.until(ExpectedConditions.alertIsPresent());
-//		      wd.switchTo().alert().accept();
-//		
-//	    	  
-//	      }catch (NoAlertPresentException Ex) {
-//	    	  
-//	       throw new WebDriverException("Alert is not there");
-//	      }
-//	      WebDriverWait wait = new WebDriverWait(wd, 2);
-//	      wait.until(ExpectedConditions.alertIsPresent());
-//	      try{
-//	    	    Alert alert = wd.switchTo().alert();
-//	    	    alert.accept();
-//	    	}
-//	    	catch (NoAlertPresentException e) {
-//	    	    System.out.println("No alert");
-//	    	}
-//	      WebDriverWait wait = new WebDriverWait(wd, 2);
-//	      if(wait.until(ExpectedConditions.alertIsPresent())==null)
-//	          System.out.println("alert was not present");
-//	      else
-//	    	  wd.switchTo().alert().accept();
-//	          System.out.println("alert was present");
-//	          
-//	          
-//	          
-//	      Thread.sleep(2000);
-//	      
-//	     
-//	      
-//	      wd.findElement(By.name("email_address")).sendKeys("rajatsalvi6@gmail.com");
-//	      wd.findElement(By.name("company")).sendKeys("Karma");
-//	      wd.findElement(By.name("street_address")).sendKeys("38/B Twin House Colony, Near Fire Station, LBS Marg, Vikhroli-west");
-//	      wd.findElement(By.name("suburb")).sendKeys("Mumbai Suburban");
-//	      wd.findElement(By.name("postcode")).sendKeys("400079");
-//	      wd.findElement(By.name("city")).sendKeys("Mumbai");
-//	      wd.findElement(By.name("state")).sendKeys("Maharashtra");
-//	      Select Dropdown = new Select (wd.findElement(By.name("country")));
-//	      Dropdown.selectByIndex(99);
-//	      wd.findElement(By.name("telephone")).sendKeys("+91 9768118799");
-//	      wd.findElement(By.name("password")).sendKeys("st.josephs");
-//	      wd.findElement(By.name("confirmation")).sendKeys("st.josephs");
-//	      
+      
 	      ((JavascriptExecutor)wd).executeScript("window.open()");
 	      ArrayList<String> tab = new ArrayList<String>(wd.getWindowHandles());
 	      wd.switchTo().window(tab.get(1));
@@ -113,14 +66,7 @@ public class Form {
 	      wd.quit();
 		}
 
-//	private static boolean isAlertPresent() {
-//		// TODO Auto-generated method stub
-//		 WebDriverWait wait = new WebDriverWait(wd,10);
-//	      wait.until(ExpectedConditions.alertIsPresent());
-//	      wd.switchTo().alert().accept();
-//		return true;
-//	}
-////	
+
 
 	}
 
